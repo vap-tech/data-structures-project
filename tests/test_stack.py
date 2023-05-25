7ace7f96e8d70f03a9f4c56260cf9dce08c982d9
Vitaliy Petrenko <mail@v-petrenko.ru>
2023-05-25 23:13:39 +0300

    test ok cov = 74

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,23 @@
-"""Здесь надо написать тесты с использованием unittest для модуля stack."""
+"""Здесь тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Stack
+
+
+class TestStack(unittest.TestCase):
+    def test_stack(self):
+        # Пишем нужное количество проверок
+        stack = Stack()
+        stack.push('data1')
+        stack.push('data2')
+        stack.push('data3')
+        self.assertEqual(stack.top.data, 'data3')
+        self.assertEqual(stack.top.next_node.data, 'data2')  # data2
+        self.assertEqual(stack.top.next_node.next_node.data, 'data1')  # data1
+        self.assertEqual(stack.top.next_node.next_node.next_node, None)  # None
+
+        with self.assertRaises(AttributeError):
+            print(stack.top.next_node.next_node.next_node.data)
+
+
+if __name__ == '__main__':
+    unittest.main()
